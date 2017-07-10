340aa45434a85fd1ce6bfd77f274ed523feac345
artemsyvak <artsyvak@gmail.com>
2017-07-10 19:30:26 +0300

    added service user and product

--- src/main/java/pack/entity/User.java
+++ src/main/java/pack/entity/User.java
@@ -2,6 +2,7 @@ package pack.entity;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
